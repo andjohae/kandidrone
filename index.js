34434d75f1f91b67288b35034ee26f712404c17a
andjohae <andenj@hotmail.com>
2015-03-06 12:59:28 +0100

    Created instances of the client, control and mission objects.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+//var kandiDrone = exports;
 var arDrone = require('ar-drone');
 var autonomy = require('ardrone-autonomy');
 
-console.log('Hello world')
+var client = new arDrone.createClient();
+var control = new autonomy.control(client);
+var mission = new autonomy.createMission(client);
+
+console.log('Hello world');
