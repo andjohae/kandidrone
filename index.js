6e715ed93372bb37d46f2af4953cee7ea2bb1583
andjohae <andenj@hotmail.com>
2015-03-26 13:37:50 +0100

    Updated index file with support for the new tagSearch module.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-//var kandiDrone = exports;
+var kandiDrone = exports;
+
+exports.tagSearch = require('./lib/tagSearch')
+
 var arDrone = require('ar-drone');
 var autonomy = require('ardrone-autonomy');
 
-var client = new arDrone.createClient();
-var ctrl = new autonomy.control(client);
-var mission = new autonomy.createMission(client);
-
-client.resume();
-client.on('batteryChange', function (battery) {
-    console.log(battery)
-});
+exports.createTagSearch = function (client, controller, mission, options) {
+    var client = client || arDrone.createClient();
+    var controller = controller || autonomy.control(client, options);
+    var mission = mission || autnonomy.createMission(client, controller, options);
+    return new kandiDrone.tagSearch(client, controller, mission, options);
+}
