44feeb4dd4061f0e3ba3f3091565788bf48bfe30
andjohae <andenj@hotmail.com>
2015-03-27 14:15:36 +0100

    Tested the tagDetected event from tagSearch

--- examples/testTagArray.js
+++ examples/testTagArray.js
@@ -3,5 +3,13 @@ var tagSearch = kandiDrone.createTagSearch();
 
 var tag = {x: 0.5, y:1.2};
 
-tagSearch.evalUpdArray(tag);
-console.log(tagSearch.getTagArray());
+tagSearch.on('tagConfirmed', function () {
+    console.log('Success!');
+    console.log(tagSearch.getTagArray());
+})
+
+for (var i = 0; i<20; i++) {
+    tagSearch.evalUpdArray(tag);
+}
+
+//console.log(tagSearch.getTagArray());
