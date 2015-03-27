c639bef961d35438bcde4017261283cb406fb739
Joachim <jocke.benjaminsson@hotmail.com>
2015-03-27 14:01:33 +0100

    Added controller to createTagSearch

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ exports.flightRoute = require('./lib/flightRoute')
 var arDrone = require('ar-drone');
 var autonomy = require('ardrone-autonomy');
 
-exports.createTagSearch = function (client, options) {
+exports.createTagSearch = function (client, controller options) {
     var client = client || arDrone.createClient(options);
+    var controller = controller || new autonomy.Controller(client, options);
     var camera = new autonomy.Camera(options);
-    return new kandiDrone.tagSearch(client, camera, options);
+    return new kandiDrone.tagSearch(client, camera, controller, options);
 }
