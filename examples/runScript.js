3d5e95894f410c4b97a716c0ac80adbb7cbfb20a
andjohae <andenj@hotmail.com>
2015-03-31 13:16:16 +0200

    Moved the user prompt to a separate module

--- examples/runScript.js
+++ examples/runScript.js
@@ -1,10 +1,16 @@
+// Required modules:
 var arDrone = require('ar-drone'),
     autonomy = require('ardrone-autonomy'),
     kandiDrone = require('..')
 ;
-
+// Initiate and connect the different objects.
 var client = arDrone.createClient(),
     controller = autonomy.control(client),
-    tagSearch = kandiDrone.createTagSearch(client, controller),
-    kandiBrain = kandiDrone.createKandiBrain(client, controller, tagSearch)
-;
+    tagSearch = kandiDrone.createTagSearch(client, controller);
+    //kandiBrain = kandiDrone.createKandiBrain(client, controller, tagSearch);
+// Get constants from the arDrone module.
+var constants = require('ar-drone/lib/constants');
+// Get user input to define the search area and optionally the number of tags
+// to detect.
+var kandiPrompt = require('../lib/kandiPrompt');
+var userInput = new kandiPrompt().getUI();
