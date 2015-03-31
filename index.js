0df2346041b10aa688a23f2ac13323be61ed6e90
andjohae <andenj@hotmail.com>
2015-03-31 16:54:55 +0200

    Fixed minor bugs such as syntax errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var kandiDrone = exports;
 
 exports.tagSearch = require('./lib/tagSearch')
+exports.kandiBrain = require('./lib/kandiBrain')
 
 var arDrone = require('ar-drone');
 var autonomy = require('ardrone-autonomy');
@@ -12,9 +13,9 @@ exports.createTagSearch = function (client, controller, camera, options) {
     return new kandiDrone.tagSearch(client, camera, controller, options);
 }
 
-exports.createKandiBrain = function(client, controller, tagSearch, options) {
+exports.createKandiBrain = function (client, controller, tagSearch, options) {
 	var client = client || arDrone.createClient(options);
     var controller = controller || new autonomy.Controller(client, options);
     var tagSearch = tagSearch || kandiDrone.createTagSearch(client, controller);
-    return new kandiDrone.createKandiBrain(client, controller, tagSearch, options);
+    return new kandiDrone.kandiBrain(client, controller, tagSearch, options);
 }
